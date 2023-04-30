ba1edc0d33a9f134c47d1f181333a8f381a42ff2
Dr. Thai Tran <tnmthai@gmail.com>
2023-04-30 13:56:19 +1200

    updated

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	
+	$to = "your_email@example.com"; // replace with your email address
+	$subject = "New message from $name";
+	$body = "Name: $name\nEmail: $email\n\n$message";
+	
+	if (mail($to, $subject, $body)) {
+		echo "Message sent successfully.";
+	} else {
+		echo "Message delivery failed.";
+	}
+}
+?>
